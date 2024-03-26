aac2bebe516838c5a73965e441e9c5329bc4dcef
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2024-03-26 00:44:40 -0300

    a

--- src/coleccionPiezas.cpp
+++ src/coleccionPiezas.cpp
@@ -11,11 +11,32 @@ TColeccionPiezas crearColeccionPiezasVacia(){
 }
 
 void insertarPiezaColeccionPiezas(TColeccionPiezas &coleccionPiezas, TPieza pieza){
-    
+    TColeccionPiezas nuevo = new rep_coleccionpiezas;
+
+    if(coleccionPiezas = NULL){
+        nuevo->pieza = pieza;
+        nuevo->sig = NULL;
+        coleccionPiezas = nuevo;
+    }else if(idTPieza(pieza) < idTPieza(coleccionPiezas->pieza)){
+        nuevo->pieza = pieza;
+        nuevo->sig = coleccionPiezas;
+        coleccionPiezas = nuevo;        
+    }else{
+        TColeccionPiezas aux = coleccionPiezas;
+        int id = idTPieza(pieza);
+        while ((aux->sig != NULL) && idTPieza(aux->sig->pieza) < id){
+            aux = aux->sig;
+        }
+        nuevo->sig = aux->sig;
+        aux->sig = nuevo;
+    }
 }
 
 void imprimirColeccionPiezas(TColeccionPiezas coleccionPiezas){
-   
+   while (coleccionPiezas->sig != NULL){
+    imprimirTPieza(coleccionPiezas->pieza);
+    coleccionPiezas = coleccionPiezas->sig;
+   }
 }
 
 bool esVaciaColeccionPiezas(TColeccionPiezas piezas){
@@ -35,5 +56,11 @@ void removerPiezaColeccionPiezas(TColeccionPiezas &coleccionPiezas, int idPieza)
 }
 
 void liberarColeccionPiezas(TColeccionPiezas &coleccionPiezas){
-    
+    TColeccionPiezas aux = coleccionPiezas->sig;
+    while (aux != NULL){
+        liberarTPieza(coleccionPiezas->pieza);
+        delete coleccionPiezas;
+        coleccionPiezas = aux;
+        aux = aux->sig ;
+    }
 }
