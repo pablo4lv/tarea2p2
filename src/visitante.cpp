4fb215204565a9a131bf8653c9bd8f2eb79793f6
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2024-04-04 02:06:03 -0300

    a

--- src/visitante.cpp
+++ src/visitante.cpp
@@ -1,11 +1,19 @@
 #include "../include/visitante.h"
 
 struct rep_visitante {
-    
+    int id;
+    char nombre[MAX_NOMBRE];
+    char apellido[MAX_APELLIDO];
+    int edad;
 };
 
 TVisitante crearTVisitante(int id, const char nombre[MAX_NOMBRE], const char apellido[MAX_APELLIDO], int edad){
-    return NULL; 
+    TVisitante visitante = new rep_visitante;
+    visitante->id = id;
+    strcpy(visitante->nombre,nombre);
+    strcpy(visitante->apellido,apellido);
+    visitante->edad = edad;
+    return visitante; 
 }
 
 void nombreTVisitante(TVisitante visitante, char nombre[MAX_NOMBRE]){
@@ -25,7 +33,8 @@ int edadTVisitante(TVisitante visitante){
 }
 
 void liberarTVisitante(TVisitante &visitante){
-
+    delete visitante;
+    visitante = NULL;
 }
 
 TVisitante copiarTVisitante(TVisitante visitante){
@@ -33,6 +42,8 @@ TVisitante copiarTVisitante(TVisitante visitante){
 }
 
 void imprimirTVisitante(TVisitante visitante){
-
+    printf("Visitante %s %s\n", visitante->nombre, visitante->apellido);
+    printf("Id: %i\n", visitante->id);
+    printf("Edad: %i\n", visitante->edad);
 }
 
