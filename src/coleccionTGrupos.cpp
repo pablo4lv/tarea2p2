ea607bc68bc1932583d3076cc524fe6112bf7b73
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2024-04-18 16:10:01 -0300

    a

--- src/coleccionTGrupos.cpp
+++ src/coleccionTGrupos.cpp
@@ -83,12 +83,14 @@ void imprimirTColeccionTGrupos(TColeccionTGrupos coleccion){
 }
 
 void imprimirInvertidoTColeccionTGrupos(TColeccionTGrupos coleccion){
-	nodo* actual = coleccion->ultimo;
-	printf("Coleccion de grupos:\n");
-	while (actual != NULL){
-		printf("Grupo con edad promedio %.2f:\n", edadPromedioTGrupoABB(actual->grupo));
-		imprimirTGrupoABB(actual->grupo);
-		actual = actual->ant;
+	if(coleccion != NULL && coleccion->ultimo != NULL){
+		nodo* actual = coleccion->ultimo;
+		printf("Coleccion de grupos:\n");
+		while (actual != NULL){
+			printf("Grupo con edad promedio %.2f:\n", edadPromedioTGrupoABB(actual->grupo));
+			imprimirTGrupoABB(actual->grupo);
+			actual = actual->ant;
+		}
 	}
 }
 
